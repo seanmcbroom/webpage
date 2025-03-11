790edb7633b9b6adee18e9f39c97b7abfd7053e6
Sean McBroom <sean.mcbroom@outlook.com>
2025-03-11 17:38:35 -0500

    page: add psa message for development notice

--- frontend/src/components/Layout/Page/Page.vue
+++ frontend/src/components/Layout/Page/Page.vue
@@ -1,12 +1,14 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n'
+import PSABanner from '../PSABanner/PSABanner.vue';
 
 const { t } = useI18n()
 </script>
 
 <template>
   <div class="flex flex-col min-h-[calc(100vh)]">
+    <PSABanner :message="t('psa-message')" />
     <main
       class="flex flex-col items-center h-[100%] min-h-[calc(100vh)] bg-[var(--color-background)]"
     >
