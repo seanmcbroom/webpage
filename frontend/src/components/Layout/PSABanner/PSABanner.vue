a0070a98effeb594c4561cf479f7cf450e86a71f
Sean McBroom <sean.mcbroom@outlook.com>
2025-03-11 18:11:52 -0500

    fix(frontend): css & translation improvements

--- frontend/src/components/Layout/PSABanner/PSABanner.vue
+++ frontend/src/components/Layout/PSABanner/PSABanner.vue
@@ -5,27 +5,7 @@ defineProps<{
 </script>
 
 <template>
-  <div class="psa-banner">
+  <div class="text-white flex items-center justify-center min-h-[40px] bg-[var(--color-accent)] px-4">
     <a>{{ message }}</a>
   </div>
 </template>
-
-<style scoped>
-.psa-banner {
-  background-color: var(--color-accent);
-  color: #fff;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  min-height: 35px;
-  text-align: center;
-  padding: 5px 0px;
-}
-
-@media (max-width: 900px) {
-  .psa-banner {
-    padding: 10px 10px;
-  }
-}
-</style>
