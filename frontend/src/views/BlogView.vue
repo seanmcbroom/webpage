2861254724040585bd034a36589982c091de174e
Sean McBroom <sean.mcbroom@outlook.com>
2025-10-10 14:38:50 -0500

    feat: add home return button to blog posts page

--- frontend/src/views/BlogView.vue
+++ frontend/src/views/BlogView.vue
@@ -1,11 +1,15 @@
 <template>
-  <div class="flex flex-col my-auto items-center">
-    <ul class="space-y-1">
-      <li v-for="post in posts" :key="post.slug" class="space-x-2">
-        <a :href="`/posts/${post.slug}`" target="_blank">{{ post.metadata.title }}</a>
-        <a v-if="post.metadata.upload" class="text-gray-500 text-sm">{{ post.metadata.upload }}</a>
-      </li>
-    </ul>
+  <div class="flex flex-col my-auto items-center text-center space-y-6">
+    <div class="flex flex-col items-center">
+      <ul class="space-y-1">
+        <li v-for="post in posts" :key="post.slug" class="space-x-2">
+          <a :href="`/posts/${post.slug}`" target="_blank">{{ post.metadata.title }}</a>
+          <a v-if="post.metadata.upload" class="text-gray-500 text-sm">{{ post.metadata.upload }}</a>
+        </li>
+      </ul>
+    </div>
+
+    <a href="/">{{ t('home') }}</a>
   </div>
 </template>
 
@@ -15,7 +19,7 @@ import { useI18n } from 'vue-i18n'
 import { getAllPosts } from '@/utils/blog-posts'
 import { useDocumentTitle } from '@/composables/useDocumentTitle'
 
-const { locale } = useI18n()
+const { t, locale } = useI18n()
 
 const posts = computed(() => getAllPosts(locale.value))
 
