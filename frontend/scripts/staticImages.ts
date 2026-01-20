5c24e7020282884208e0142c26a9472ce3a9d6e3
Sean McBroom <sean.mcbroom@outlook.com>
2026-01-19 22:57:14 -0600

    chore: lint fixes

--- frontend/scripts/staticImages.ts
+++ frontend/scripts/staticImages.ts
@@ -1,12 +1,12 @@
 import fs from "node:fs";
 import path from "node:path";
 
-const srcDir = path.resolve("./src/assets/images"); 
+const srcDir = path.resolve("./src/assets/images");
 const destDir = path.resolve("./dist/assets/images");
 
 fs.mkdirSync(destDir, { recursive: true });
 
-fs.readdirSync(srcDir).forEach(file => {
+fs.readdirSync(srcDir).forEach((file) => {
   const srcFile = path.join(srcDir, file);
   const destFile = path.join(destDir, file);
   fs.copyFileSync(srcFile, destFile);
